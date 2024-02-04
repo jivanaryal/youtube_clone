200f9b066baf836bd2a98f007a7fa52a29d44d42
jivan aryal <jeevanaryal7947@gmail.com>
2024-02-04 22:12:52 +0545

    removed

--- yt_frontend/src/hoc/Layout.jsx
+++ yt_frontend/src/hoc/Layout.jsx
@@ -1,6 +1,4 @@
-import { Outlet } from "react-router-dom";
 import Navbar from "../components/navigation/Navbar";
-import Sidebar from "../components/navigation/Sidebar";
 
 const Layout = () => {
   return (
